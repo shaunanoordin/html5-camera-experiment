6cc4cda3fddfc1cb26247a81bc6f4af21d5b8c58
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2017-05-12 17:06:08 +0100

    Basic video and image capture (#1)
    * Basic video setup
    
    * Cleanup
    
    * Goodbye importExample
    
    * Video capture!

--- app/main.js
+++ app/main.js
@@ -42,33 +42,90 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
 	"use strict";
 
-	var _importExample = __webpack_require__(1);
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } } /*  
-	                                                                                                                                                          Starter JS
-	                                                                                                                                                          ==========
-	                                                                                                                                                          
-	                                                                                                                                                          Starter tempalte for JS projects
-	                                                                                                                                                          
-	                                                                                                                                                          (Shaun A. Noordin || shaunanoordin.com || 20160509)
-	                                                                                                                                                          ********************************************************************************
-	                                                                                                                                                           */
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	/*  
+	Starter JS
+	==========
+
+	Starter tempalte for JS projects
+
+	(Shaun A. Noordin || shaunanoordin.com || 20160509)
+	********************************************************************************
+	 */
 
 	/*  Primary App Class
 	 */
 	//==============================================================================
-	var App = function App() {
-	  _classCallCheck(this, App);
+	var App = function () {
+	  function App() {
+	    var _this = this;
 
-	  var importExample = new _importExample.ImportExample("HI THERE");
+	    _classCallCheck(this, App);
 
-	  this.console = document.getElementById("console");
-	  this.console.innerHTML = "This is a starter template for JS projects. <br>" + importExample.getText();
-	};
+	    this.html = {
+	      video: document.getElementById("video"),
+	      canvas: document.getElementById("canvas"),
+	      console: document.getElementById("console"),
+	      captureButton: document.getElementById("capture-button")
+	    };
+
+	    this.context = this.html.canvas.getContext("2d"), this.videoWidth = 0;
+	    this.videoHeight = 0;
+
+	    navigator.mediaDevices && navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function (stream) {
+	      console.log("VIDEO OK", stream);
+	      _this.print("VIDEO OK");
+
+	      _this.html.video.srcObject = stream;
+	      _this.html.video.onloadedmetadata = function (e) {
+	        _this.html.video.play();
+	        _this.videoWidth = _this.html.video.videoWidth;
+	        _this.videoHeight = _this.html.video.videoHeight;
+	        _this.html.canvas.width = _this.videoWidth;
+	        _this.html.canvas.height = _this.videoHeight;
+	        _this.print('Video size is ' + _this.videoWidth + 'x' + _this.videoHeight);
+	      };
+	    }).catch(function (err) {
+	      console.error(err);
+	      _this.print("VIDEO ERROR");
+	    });
+
+	    this.html.captureButton.onclick = function () {
+	      _this.captureImage();
+	      _this.print("SNAPSHOT!");
+	    };
+	  }
+
+	  _createClass(App, [{
+	    key: "captureImage",
+	    value: function captureImage() {
+	      if (this.videoWidth > 0 && this.videoHeight > 0) {
+	        this.context.drawImage(this.html.video, 0, 0, this.videoWidth, this.videoHeight);
+	      }
+	    }
+	  }, {
+	    key: "print",
+	    value: function print(msg) {
+	      var p = document.createElement("p");
+	      p.innerHTML = msg;
+	      this.html.console.appendChild(p);
+	    }
+	  }, {
+	    key: "clear",
+	    value: function clear() {
+	      this.html.console.innerHTML = "";
+	    }
+	  }]);
+
+	  return App;
+	}();
 	//==============================================================================
 
 	/*  Initialisations
@@ -80,44 +137,6 @@
 	window.onload = function () {
 	  window.app = new App();
 	};
-	//==============================================================================
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	/*  Example Class
-	    Demonstrates ES6 import function in main.js
-	 */
-	//==============================================================================
-	var ImportExample = exports.ImportExample = function () {
-	  function ImportExample() {
-	    var txt = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "DEFAULT TEXT";
-
-	    _classCallCheck(this, ImportExample);
-
-	    this.txt = txt;
-	  }
-
-	  _createClass(ImportExample, [{
-	    key: "getText",
-	    value: function getText() {
-	      return "An ImportExample class was imported and has the text: " + this.txt;
-	    }
-	  }]);
-
-	  return ImportExample;
-	}();
 	//==============================================================================
 
 /***/ }
--- src/main.js
+++ src/main.js
@@ -8,19 +8,63 @@ Starter tempalte for JS projects
 ********************************************************************************
  */
 
-import {ImportExample} from "./importExample.js";
-
 /*  Primary App Class
  */
 //==============================================================================
 class App {
   constructor() {
-    let importExample = new ImportExample("HI THERE");
+    this.html = {
+      video: document.getElementById("video"),
+      canvas: document.getElementById("canvas"),
+      console: document.getElementById("console"),
+      captureButton: document.getElementById("capture-button"),
+    };
     
-    this.console = document.getElementById("console");
-    this.console.innerHTML =
-      "This is a starter template for JS projects. <br>" +
-      importExample.getText();
+    this.context = this.html.canvas.getContext("2d"),
+    this.videoWidth = 0;
+    this.videoHeight = 0;
+    
+    navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+    .then((stream) => {
+      console.log("VIDEO OK", stream);
+      this.print("VIDEO OK");
+      
+      this.html.video.srcObject = stream;
+      this.html.video.onloadedmetadata = (e) => {
+        this.html.video.play();
+        this.videoWidth = this.html.video.videoWidth;
+        this.videoHeight = this.html.video.videoHeight;
+        this.html.canvas.width = this.videoWidth;
+        this.html.canvas.height = this.videoHeight;
+        this.print('Video size is ' + this.videoWidth + 'x' + this.videoHeight);
+      };
+    })
+    .catch((err) => {
+      console.error(err);
+      this.print("VIDEO ERROR");
+    });
+    
+    this.html.captureButton.onclick = () => {
+      this.captureImage();
+      this.print("SNAPSHOT!");
+    };
+  }
+  
+  captureImage() {
+    if (this.videoWidth > 0 && this.videoHeight > 0) {
+      this.context.drawImage(this.html.video, 0, 0, this.videoWidth, this.videoHeight);
+    }
+  }
+  
+  print(msg) {
+    const p = document.createElement("p");
+    p.innerHTML = msg;
+    this.html.console.appendChild(p);
+  }
+  
+  clear() {
+    this.html.console.innerHTML = "";
   }
 }
 //==============================================================================
